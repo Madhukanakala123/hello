87085de1d9ff836a8a003064ba312c50e6fda27c
Madhukanakala123 <106221509+Madhukanakala123@users.noreply.github.com>
2023-07-13 11:34:37 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,6 @@
+public class ex {
+    public static void main(String args[]){
+        System.out.println("hello world");
+        System.out.println("Devops lab");  //changes made in this 
+    }
+}
